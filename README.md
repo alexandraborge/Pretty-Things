c73c8e13082baa604abe79580bf3aea98e77108e
Alexandra Borge <alexandraroseingram@gmail.com>
2018-03-29 12:53:37 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pretty-Things
+
+A blog site where anyone can write about the beautiful things that inspire them.
--- main.rb
+++ main.rb
@@ -7,3 +7,7 @@ end
 get '/new' do
   erb :new_post
 end
+
+post '/' do
+  #create a new html file for each form submitted and send the info submitted in the params to the home page
+end
